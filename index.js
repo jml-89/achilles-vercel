2be5f7dd453231472cdfe614e19b6a813cb728d2
Jesper Lillie <jesper.lillie@gmail.com>
2024-08-19 13:01:12 +1000

    Response A code block 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Response A, code block 1");
 });
 
 app.listen(3000, () => { console.log("App started"); });
