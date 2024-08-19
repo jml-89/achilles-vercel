6a2836a209dbfcecc2104e59ac171388a1ef76ec
Jesper Lillie <jesper.lillie@gmail.com>
2024-08-19 13:53:55 +1000

    index re-added

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send(`Response A, code block 1. NODE_ENV: "${process.env.NODE_ENV}"`);
+});
+
+app.listen(3000, () => { console.log("App started"); });
